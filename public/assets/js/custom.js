27ddbdc6dbe71c4a1b8224247aaee2b09cdca838
patriciax <yively.pa@gmail.com>
2022-01-20 15:32:40 -0500

    fix home

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -51,6 +51,43 @@ $(document).ready(function () {
             // instead of a settings object
         ],
     });
+    $(".slider-ads").slick({
+        dots: false,
+        arrows:false,
+
+        speed: 300,
+        autoplay: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ],
+    });
 
    $(".see-more").on("click", function () {
         if (!$(".more-info-service-1").hasClass("is-active")) {
