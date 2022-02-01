53ea16f4f99c9ebbc017b41caa5445a2774f11d5
willian rodriguez <rodriguezwillian95@gmail.com>
2022-02-01 19:41:39 -0400

    cambios

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -63,9 +63,9 @@
 
             @endif
 
-            @if(App\Models\Blog::orderBy("id", "desc")->first())
+            @if(App\Models\Blog::orderBy("id", "desc")->skip(1)->first())
                 @php
-                    $blog = App\Models\Blog::orderBy("id", "desc")->first();
+                    $blog = App\Models\Blog::orderBy("id", "desc")->skip(1)->first();
                 @endphp
                 <div class="blog-item" data-bs-toggle="modal" data-bs-target=".modal-blog">
                     <img src="{{ $blog->image }}" alt="">
